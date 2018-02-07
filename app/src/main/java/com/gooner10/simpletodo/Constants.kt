2f45dc0cbacaab9dfc24b3cb0f4363ce5ac79d7f
Birat Rai <biratrai@hotmail.com>
2018-02-06 23:09:37 -0500

    converting to realm for constant class

---
+++ app/src/main/java/com/gooner10/simpletodo/Constants.kt
@@ -0,0 +1,10 @@
+package com.gooner10.simpletodo
+
+/**
+ * Application Constants
+ */
+object Constants {
+    val ITEM_NAME = "itemName"
+    val SCENE_TRANSITION = "toDoTransition"
+    val TODO_DIALOG_FRAGMENT_TAG = "Add new ToDo"
+}
