c367bb4065580450916b839e541ad4f28d31e00d
Birat Rai <biratrai@hotmail.com>
2018-02-09 10:46:43 -0500

    reverting to java class

---
+++ app/src/main/java/com/gooner10/simpletodo/edit/EditActivity.java
@@ -0,0 +1,61 @@
+package com.gooner10.simpletodo.edit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.gooner10.simpletodo.Constants;
+import com.gooner10.simpletodo.R;
+import com.gooner10.simpletodo.ToDoApplication;
+import com.gooner10.simpletodo.databinding.ActivityEditBinding;
+import com.gooner10.simpletodo.model.ToDoModel;
+import com.gooner10.simpletodo.model.ToDoRepository;
+
+import javax.inject.Inject;
+
+public class EditActivity extends AppCompatActivity {
+    private EditText editText;
+    private ToDoModel toDoModel;
+
+    @Inject
+    ToDoRepository toDoRepository;
+
+    public static Intent getEditActivity(Activity activity, String id) {
+        Intent intent = new Intent(activity, EditActivity.class);
+        intent.putExtra(Constants.INSTANCE.getITEM_NAME(), id);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        ToDoApplication.getToDoApplication().getComponent().inject(this);
+
+        ActivityEditBinding editBinding = DataBindingUtil.setContentView(this, R.layout.activity_edit);
+
+        String itemName = (String) getIntent().getExtras().get(Constants.INSTANCE.getITEM_NAME());
+
+        toDoModel = toDoRepository.getRealmModel(itemName);
+
+        editText = editBinding.editToDoText;
+        editText.setText(toDoModel.getToDoName());
+
+        Button editSaveBtn = editBinding.editSaveBtn;
+        editSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                toDoRepository.editToDo(toDoModel, editText.getText().toString());
+                supportFinishAfterTransition();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/gooner10/simpletodo/todo/ToDoActivity.java
@@ -0,0 +1,143 @@
+package com.gooner10.simpletodo.todo;
+
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+
+import com.gooner10.simpletodo.Constants;
+import com.gooner10.simpletodo.R;
+import com.gooner10.simpletodo.ToDoApplication;
+import com.gooner10.simpletodo.databinding.ActivityMainBinding;
+import com.gooner10.simpletodo.edit.EditActivity;
+import com.gooner10.simpletodo.model.ToDoModel;
+import com.gooner10.simpletodo.model.ToDoRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class ToDoActivity extends AppCompatActivity implements ToDoItemsAdapter.OnItemClickListener,
+        ToDoContract.View {
+    public static final String LOG_TAG = ToDoActivity.class.getSimpleName();
+    private ToDoItemsAdapter toDoItemsAdapter;
+    private ToDoContract.UserActionsListener mActionsListener;
+    private FloatingActionButton fab;
+    private ActivityMainBinding mainBinding;
+
+    @Inject
+    ToDoRepository toDoRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        ToDoApplication.getToDoApplication().getComponent().inject(this);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mActionsListener = new ToDoPresenter(this, toDoRepository);
+
+        Toolbar toolbar = mainBinding.toolbar;
+        setSupportActionBar(toolbar);
+
+        RecyclerView mRecyclerView = mainBinding.layoutContentMain.recyclerview;
+
+        toDoItemsAdapter = new ToDoItemsAdapter(this);
+        mRecyclerView.setAdapter(toDoItemsAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        configureRecyclerViewSwipe(mRecyclerView);
+
+        configureFabButton();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mActionsListener.loadToDo();
+    }
+
+    private void configureRecyclerViewSwipe(RecyclerView mRecyclerView) {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                  RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                mActionsListener.deleteToDo(toDoItemsAdapter.getItem(viewHolder.getAdapterPosition()));
+                mActionsListener.loadToDo();
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    /**
+     * Circular Reveal Animation added for the fab button in onClick
+     */
+    private void configureFabButton() {
+        fab = mainBinding.fab;
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Adding Circular Reveal to the fab button
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                        // get the center for the clipping circle
+                        int cx = fab.getMeasuredWidth() / 2;
+                        int cy = fab.getMeasuredHeight() / 2;
+
+                        // get the final radius for the clipping circle
+                        int finalRadius = Math.max(fab.getWidth(), fab.getHeight()) / 2;
+                        Animator anim = ViewAnimationUtils.createCircularReveal(fab, cx, cy, 0, finalRadius);
+                        anim.start();
+                    }
+                    ToDoDialogFragment mToDoDialogFragment = ToDoDialogFragment.newInstance(R.string.add_new_todo);
+                    mToDoDialogFragment.show(getSupportFragmentManager(), Constants.INSTANCE.getTODO_DIALOG_FRAGMENT_TAG());
+                }
+            });
+        }
+    }
+
+    public void addNewToDo(String newToDo) {
+        mActionsListener.addNewToDo(newToDo);
+    }
+
+    @Override
+    public void showToDoUi(List<ToDoModel> mToDoList) {
+        mainBinding.layoutContentMain.setUserList(mToDoList);
+        toDoItemsAdapter.setItems(mToDoList);
+    }
+
+    @Override
+    public void updateChanges() {
+        mActionsListener.loadToDo();
+    }
+
+    @Override
+    public void onItemClick(ToDoItemsAdapter.ItemHolder item, int position) {
+        Intent intent = EditActivity.getEditActivity(this, toDoItemsAdapter.getItem(position).getId());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            ActivityOptionsCompat options = ActivityOptionsCompat.
+                    makeSceneTransitionAnimation(this, item.textItemName, Constants.INSTANCE.getSCENE_TRANSITION());
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+    }
+}
+
