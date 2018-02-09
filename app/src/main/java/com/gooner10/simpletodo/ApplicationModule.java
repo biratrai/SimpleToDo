d2acd40596b428bbb878baee881d79beb27a0701
Birat Rai <biratrai@hotmail.com>
2018-02-09 17:14:19 -0500

    fixing the applicationMoudle with 2.10 dagger changes

--- app/src/main/java/com/gooner10/simpletodo/ApplicationModule.java
+++ app/src/main/java/com/gooner10/simpletodo/ApplicationModule.java
@@ -1,11 +1,7 @@
 package com.gooner10.simpletodo;
 
-import android.app.Application;
-
 import com.gooner10.simpletodo.model.ToDoRepository;
 import com.gooner10.simpletodo.model.ToDoRepositoryImpl;
-
-import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -17,27 +13,14 @@ import io.realm.Realm;
  */
 @Module
 public class ApplicationModule {
-    private final Application mApplication;
-
-    public ApplicationModule(Application application) {
-        mApplication = application;
-    }
-
-//    @Provides
-//    @Singleton
-//    public Application provideApplicationContext() {
-//        return mApplication;
-//    }
 
     @Provides
-    @Singleton
     Realm provideRealm() {
         return Realm.getDefaultInstance();
     }
 
     @Provides
-    @Singleton
-    public ToDoRepository provideRepository(Realm realm) {
+    ToDoRepository provideRepository(Realm realm) {
         return new ToDoRepositoryImpl(realm);
     }
 }
