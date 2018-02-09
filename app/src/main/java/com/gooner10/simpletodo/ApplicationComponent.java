f15b656b3673c8227477cfeffb23e30c89a69305
Birat Rai <biratrai@hotmail.com>
2018-02-09 17:13:38 -0500

    finxing the compoenent for dagger injection

--- app/src/main/java/com/gooner10/simpletodo/ApplicationComponent.java
+++ app/src/main/java/com/gooner10/simpletodo/ApplicationComponent.java
@@ -4,23 +4,32 @@ package com.gooner10.simpletodo;
  * Dagger Component
  */
 
+import android.app.Application;
+
 import com.gooner10.simpletodo.todo.ToDoActivity;
+import com.gooner10.simpletodo.todo.ToDoActivityBindingModule;
 
 import javax.inject.Singleton;
 
+import dagger.BindsInstance;
 import dagger.Component;
+import dagger.android.AndroidInjector;
 
 @Singleton
-@Component(modules = {ApplicationModule.class})
-public interface ApplicationComponent {
+@Component(modules = {ApplicationModule.class, ToDoActivityBindingModule.class})
+public interface ApplicationComponent extends
+        AndroidInjector<ToDoApplication> {
 
-//    @Component.Builder
-//    interface Builder {
-//        @BindsInstance
-//        Builder application(Application application);
-//
-//        ApplicationComponent build();
-//    }
+    @Override
+    void inject(ToDoApplication application);
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        ApplicationComponent.Builder application(Application application);
+
+        ApplicationComponent build();
+    }
 
     void inject(ToDoActivity toDoActivity);
 
