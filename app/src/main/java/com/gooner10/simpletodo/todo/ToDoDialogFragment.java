102baf2505ded0e0142e9dfc9e5aa618c7d26228
Birat Rai <biratrai@hotmail.com>
2018-02-09 12:50:01 -0500

    reverting to before and adding edittextinputlkayot

--- app/src/main/java/com/gooner10/simpletodo/todo/ToDoDialogFragment.java
+++ app/src/main/java/com/gooner10/simpletodo/todo/ToDoDialogFragment.java
@@ -1,16 +1,22 @@
 package com.gooner10.simpletodo.todo;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.gooner10.simpletodo.R;
+import com.gooner10.simpletodo.databinding.InputDialogBoxBinding;
+
+import hugo.weaving.DebugLog;
 
 /**
  * A To Do DialogFragment that displays Dialog
@@ -18,9 +24,9 @@ import com.gooner10.simpletodo.R;
  */
 
 public class ToDoDialogFragment extends DialogFragment {
-    private EditText mEditText;
+    private TextInputEditText mEditText;
     private static final String TITLE = "title";
-//    private InputDialogBoxBinding dialogBoxBinding;
+    private InputDialogBoxBinding dialogBoxBinding;
 
     public ToDoDialogFragment() {
     }
@@ -40,44 +46,45 @@ public class ToDoDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        dialogBoxBinding = DataBindingUtil.inflate(inflater, R.layout.input_dialog_box, container, false);
+        dialogBoxBinding = DataBindingUtil.inflate(inflater, R.layout.input_dialog_box, container, false);
         return null;
     }
 
     @Override
+    @DebugLog
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         int title = getArguments().getInt(TITLE);
-//        mEditText = dialogBoxBinding.addToDoText;
-//        final TextInputLayout mInputText = dialogBoxBinding.textInputLayout;
+        mEditText = dialogBoxBinding.addToDoText;
+        final TextInputLayout mInputText = dialogBoxBinding.textInputLayout;
         getDialog().setTitle(title);
         setCancelable(false);
         mEditText.requestFocus();
-//        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
-//        Button cancelBtn = dialogBoxBinding.cancelInputBtn;
-//        cancelBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                dismiss();
-//            }
-//        });
-//
-//        Button addBtn = dialogBoxBinding.addInputBtn;
-//        addBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (mEditText != null) {
-//                    String m_Text = mEditText.getText().toString().trim();
-//                    if (m_Text.length() == 0) {
-//                        mInputText.setError(getString(R.string.empty_input_error));
-//
-//                    } else {
-//                        ((ToDoActivity) getActivity()).addNewToDo(m_Text);
-//                        dismiss();
-//                    }
-//                }
-//            }
-//        });
+        Button cancelBtn = dialogBoxBinding.cancelInputBtn;
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        Button addBtn = dialogBoxBinding.addInputBtn;
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEditText != null) {
+                    String m_Text = mEditText.getText().toString().trim();
+                    if (m_Text.length() == 0) {
+                        mInputText.setError(getString(R.string.empty_input_error));
+
+                    } else {
+                        ((ToDoActivity) getActivity()).addNewToDo(m_Text);
+                        dismiss();
+                    }
+                }
+            }
+        });
     }
 }
