15b3423114c15a7d56d30948386dde889245ee11
Birat Rai <biratrai@hotmail.com>
2018-02-09 21:06:37 -0500

    chaning the name to Presenter

--- app/src/main/java/com/gooner10/simpletodo/todo/ToDoContract.java
+++ app/src/main/java/com/gooner10/simpletodo/todo/ToDoContract.java
@@ -18,7 +18,7 @@ public interface ToDoContract {
 //        void showEditToDo();
     }
 
-    interface UserActionsListener {
+    interface Presenter {
         /**
          * Loads @{>}
          */
