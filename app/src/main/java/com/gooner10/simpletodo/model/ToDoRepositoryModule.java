f966855c9da344ecbcefd9d1fcc1d65982e0b670
birat rai <biratrai@hotmail.com>
2018-11-07 16:49:08 -0500

    adding the ToDoRepositoryModule

--- app/src/main/java/com/gooner10/simpletodo/model/ToDoRepositoryModule.java
+++ app/src/main/java/com/gooner10/simpletodo/model/ToDoRepositoryModule.java
@@ -1,4 +1,16 @@
 package com.gooner10.simpletodo.model;
 
-public interface ToDoRepositoryModule {
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import io.realm.Realm;
+
+@Module()
+public class ToDoRepositoryModule {
+    @Provides
+    @Singleton
+    Realm provideRealm() {
+        return Realm.getDefaultInstance();
+    }
 }
